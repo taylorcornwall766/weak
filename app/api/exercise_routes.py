4474e4000c6b98ae60f6621aa172c426367ff3f6
steven cornwall <taylorchangordie@icloud.com>
2023-06-14 13:22:41 -0500

    CREATE, DELETE exercise complete

--- app/api/exercise_routes.py
+++ app/api/exercise_routes.py
@@ -2,20 +2,35 @@ from flask import Blueprint, jsonify, session, request
 from app.models.exercises import Exercise
 from app.models.db import db
 from app.forms.exercise_form import ExerciseForm
-from flask_login import login_required, current_user, login_user, logout
+from flask_login import login_required, current_user
 from app.api.auth_routes import validation_errors_to_error_messages
 
 exercise_routes = Blueprint("exercise", __name__)
 
-@exercise_routes("")
+@exercise_routes.route("")
 def get_all_exercises():
     """get all of the exercises"""
-    all_exercises = Exercise.query.all()
+    all_exercises = Exercise.query.order_by(Exercise.primary_muscle.asc(), Exercise.name.asc()).all()
 
     res = [exercise.to_dict() for exercise in all_exercises]
     return {"exercises": res}
 
-@exercise_routes("/new", methods=["POST"])
+@exercise_routes.route("/<int:exercise_id>/delete", methods=['DELETE'])
+@login_required
+def delete_exercise(exercise_id):
+    exercise_to_delete = Exercise.query.get(exercise_id)
+    if exercise_to_delete is None:
+        return {'errors': 'exercise cannot be found'}
+    exercise_dict = exercise_to_delete.to_dict()
+    if exercise_dict["authorId"] is not int(current_user.id):
+        return {'errors': 'you can only delete exercises you have posted!'}
+
+    db.session.delete(exercise_to_delete)
+    db.session.commit()
+    return {'message': 'exercise deleted'}
+
+
+@exercise_routes.route("/new", methods=["POST"])
 @login_required
 def post_exercise():
     """post an exercise and get it back"""
@@ -25,7 +40,7 @@ def post_exercise():
 
     if form.validate_on_submit():
         new_exercise = Exercise(
-            authorId = int(user_id),
+            author_id = int(user_id),
             name = form.data["name"],
             description = form.data["description"],
             primary_muscle = form.data["primary_muscle"],
---
+++ react-app/src/components/Exercises/Exercises.js
@@ -0,0 +1,3 @@
+import React, {useState, useEffect} from 'react'
+import {useSelector, useDispatch} from 'react-redux'
+
